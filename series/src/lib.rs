f1a423a5b2afafb7684efcbb0b198d3a0100ea56
Aleksei Latyshev <alex_700_95@mail.ru>
2020-03-12 14:39:21 +0300

    [series] add check for len=0

--- series/src/lib.rs
+++ series/src/lib.rs
@@ -1,4 +1,7 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
+    if len == 0 {
+        return vec!["".into(); digits.len() + 1];
+    }
     digits
         .as_bytes()
         .windows(len)
