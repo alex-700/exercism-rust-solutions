8e72267442bad0adfecac279146b19ec685dfc5c
Aleksei Latyshev <alex_700_95@mail.ru>
2020-03-12 14:40:00 +0300

    [wordy] implement exponential; refactor code

--- wordy/src/lib.rs
+++ wordy/src/lib.rs
@@ -5,15 +5,34 @@ use std::collections::HashMap;
 type OpType = dyn Fn(i32, i32) -> i32 + Sync;
 
 lazy_static! {
-    static ref NAME_TO_OP: HashMap<&'static str, Box<OpType>> = {
+    static ref NAME_TO_OP: HashMap<&'static str, (&'static str, Box<OpType>)> = {
         let mut ans = HashMap::new();
-        ans.insert("plus", Box::new(|a, b| a + b) as Box<OpType>);
-        ans.insert("minus", Box::new(|a, b| a - b) as Box<OpType>);
-        ans.insert("divided by", Box::new(|a, b| a / b) as Box<OpType>);
-        ans.insert("multiplied by", Box::new(|a, b| a * b) as Box<OpType>);
+        ans.insert("plus", ("", Box::new(|a, b| a + b) as Box<OpType>));
+        ans.insert("minus", ("", Box::new(|a, b| a - b) as Box<OpType>));
+        ans.insert("divided by", ("", Box::new(|a, b| a / b) as Box<OpType>));
+        ans.insert("multiplied by", ("", Box::new(|a, b| a * b) as Box<OpType>));
+        ans.insert(
+            "raised to the",
+            (
+                ".. power",
+                Box::new(|a: i32, b: i32| a.pow(b as u32)) as Box<OpType>,
+            ),
+        );
         ans
     };
-    static ref ALL_OPS: String = {
+    static ref MAIN_REGEX_OPS: String = {
+        let mut ans = String::new();
+        for (x, (y, _)) in NAME_TO_OP.iter() {
+            if !ans.is_empty() {
+                ans.push('|');
+            }
+            ans.push_str(x);
+            ans.push_str(r" -?\d+");
+            ans.push_str(y);
+        }
+        ans
+    };
+    static ref OPS_REGEX_OPS: String = {
         let mut ans = String::new();
         for x in NAME_TO_OP.keys() {
             if !ans.is_empty() {
@@ -24,12 +43,15 @@ lazy_static! {
         ans
     };
     static ref MAIN_REGEX: Regex = Regex::new(&format!(
-        r"^What is (-?\d+)((?: (?:{}) (?:-?\d+))*)\?$",
-        *ALL_OPS
+        r"^What is (-?\d+)(( (?:{}))*)\?$",
+        *MAIN_REGEX_OPS
     ))
     .unwrap();
-    static ref OPS_REGEX: Regex =
-        Regex::new(&format!(r"(?P<operation>{}) (?P<operand>-?\d+)", *ALL_OPS)).unwrap();
+    static ref OPS_REGEX: Regex = Regex::new(&format!(
+        r"(?P<operation>{}) (?P<operand>-?\d+)",
+        *OPS_REGEX_OPS
+    ))
+    .unwrap();
 }
 
 pub fn answer(command: &str) -> Option<i32> {
@@ -47,7 +69,8 @@ pub fn answer(command: &str) -> Option<i32> {
                     .unwrap();
                 NAME_TO_OP
                     .get(caps.name("operation").unwrap().as_str())
-                    .unwrap()(acc, operand)
+                    .unwrap()
+                    .1(acc, operand)
             })
     })
 }
